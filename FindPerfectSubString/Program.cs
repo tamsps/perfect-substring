353d7d088f9d7b8df5cc2dd9791637b56592a4dd
Tam Vu <tam.vu@hitachivantara.com>
2023-04-11 12:50:39 +0700

    Add project files.

---
+++ FindPerfectSubString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FindPerfectSubString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "1221221121";
+
+            //int t = 3;
+
+            string s = "1233452452";
+            int t = 2;
+
+            
+
+            Result.perfectSubstring(s, t);
+           
+            Console.WriteLine("Original: " + s);
+            Console.WriteLine("Times: " + t);
+            Console.WriteLine();
+            Console.WriteLine("Number of perfect substring: " + Result.subPerfect.Count);
+            foreach (var item in Result.subPerfect)
+            {
+                var start  = s.IndexOf(item);
+                var end = start + item.Length-1;
+                Console.Write(String.Format("[{0}-{1}]: ",start,end));
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FindPerfectSubString/Result.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FindPerfectSubString
+{
+    internal class Result
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+        public static List<string> subPerfect { get; set; }
+        public static int findSmallestDivisor(string s, string t)
+        {
+            var res = -1;
+
+            int time = Regex.Matches(s, t).Count;
+            if (time * t.Length == s.Length && s.IndexOf(t) == 0)
+            {
+                res = t.Length;
+            }
+            return res;
+        }
+
+        public static void perfectSubstring(string s, int k)
+        {
+            int curCount = 1;
+            int mid = 0;
+            subPerfect = new List<string>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                curCount = 1;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+
+                    if (s[i] == s[j])
+                    {
+                        curCount += 1;
+                        if (curCount == k)
+                            mid = j;
+                        if (curCount == k + 1)
+                        {
+                            curCount = 1;
+                            int realPos = j;
+                            if (CheckSubString(s, k, i, mid, j, ref realPos))
+                                subPerfect.Add(s.Substring(i, realPos - i + 1));
+                            break;
+                        }
+                    }
+                    if (curCount == k && j == s.Length - 1)
+                    {
+                        int realPos = j;
+                        if (CheckSubString(s, k, i, mid, j, ref realPos))
+                            subPerfect.Add(s.Substring(i, realPos - i + 1));
+                    }
+                }
+            }
+        }
+        public static bool CheckSubString(string s, int k, int i, int mid, int end, ref int realPos)
+        {
+
+            var sub = s.Substring(i, mid - i + 1);
+            Dictionary<char, int> charCountsSubs = sub.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+
+            var item = charCountsSubs.Any(a => a.Value != k);
+            if (item == true)
+            {
+                for (int m = mid + 1; m <= end; m++)
+                {
+                    if (charCountsSubs.ContainsKey(s[m]))
+                    {
+                        charCountsSubs[s[m]] += 1;
+                    }
+                    else
+                        charCountsSubs.Add(s[m], 1);
+                    item = charCountsSubs.Any(a => a.Value != k);
+                    if (item == false)
+                    {
+                        realPos = m;
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                realPos = mid;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
